02796b42e822113ebf45a383bb5f28914471a0ba
Prajwal Bastola <prazwal.bastola10@gmail.com>
2025-01-02 08:42:01 +0545

    signin page

--- lib/pages/signin.dart
+++ lib/pages/signin.dart
@@ -9,23 +9,168 @@ class Signin extends StatefulWidget {
 }
 
 class _SigninState extends State<Signin> {
-  final formKey = GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Form(
-          key: formKey,
-          child: Column(children: [
-            BackButton(
-              onPressed: () {
-                return context.goNamed('onboard');
-              },
+      appBar: AppBar(
+          leading: IconButton(
+        icon: Icon(Icons.arrow_back_ios_new_outlined),
+        onPressed: () {
+          context.goNamed('onboard');
+        },
+      )),
+      body: Column(
+        children: [
+          Center(
+            child: Text(
+              'Sign in now',
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
             ),
-            Text(
-              'Sign up now',
-              style: TextStyle(),
-            )
-          ])),
+          ),
+          SizedBox(height: 5),
+          Text(
+            'Please sign in to continue to our app',
+            style: TextStyle(
+              color: Colors.grey[800],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      hintText: 'Enter your email address',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.clear),
+                        onPressed: () {
+                          _emailController.clear();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      hintText: '**********',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.hide_source),
+                        onPressed: () {
+                          _passwordController.clear();
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: TextButton(
+                  onPressed: () {
+                    context.goNamed('forgotpass');
+                  },
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(color: Colors.blueAccent),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15.0),
+            child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 5, 94, 248),
+                    minimumSize: Size(double.infinity, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {}
+                },
+                child: TextButton(
+                    onPressed: () {
+                      context.goNamed('homepage');
+                    },
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(color: Colors.white),
+                    ))),
+          ),
+          SizedBox(height: 22),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Dont have an account?'),
+              TextButton(
+                onPressed: () {
+                  return context.goNamed('signup');
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.blueAccent),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('Or Connect'),
+          SizedBox(height: 100),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.network(
+                'https://img.icons8.com/?size=96&id=uLWV5A9vXIPu&format=png',
+                height: 50,
+                width: 50,
+                fit: BoxFit.contain,
+              ),
+              Image.network(
+                'https://img.icons8.com/?size=96&id=Xy10Jcu1L2Su&format=png',
+                height: 50,
+                width: 50,
+                fit: BoxFit.contain,
+              ),
+              Image.network(
+                'https://img.icons8.com/?size=128&id=119014&format=png',
+                height: 50,
+                width: 50,
+                fit: BoxFit.contain,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
